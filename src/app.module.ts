584d5eba0fe7dd8e20391244eb14b2ee74df0498
Horleryheancarh <oladipoolayinka7@gmail.com>
2024-03-30 00:40:20 +0100

    added middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './api/songs/songs.module';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // Option 1
+    consumer.apply(LoggerMiddleware).forRoutes('songs');
+    // Option 2
+    // consumer
+    //   .apply(LoggerMiddleware)
+    //   .forRoutes({ path: 'songs', method: RequestMethod.POST });
+    // Option 3
+    // consumer.apply(LoggerMiddleware).forRoutes(SongsController);
+  }
+}
